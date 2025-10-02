2380cb4a815fa1c64460db7834475d440447c07e
Connor <connorsteigerwald1@gmail.com>
2025-10-01 18:51:01 -0700

    python commit

---
+++ test.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = input(f"which course are you taking? ")
+    if x == "ECE 180":
+        print(x+ " is the best class ever")
+    else:
+        print("Bruins are the Best")
+
